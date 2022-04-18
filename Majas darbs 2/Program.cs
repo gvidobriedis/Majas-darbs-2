816b63ef307f3140eba981db8bcbf244d5cfea35
Gvido Briedis <gvidoinfo@gmail.com>
2022-04-18 23:53:34 +0300

    Majas darbs 2

--- Majas darbs 2/Program.cs
+++ Majas darbs 2/Program.cs
@@ -6,12 +6,12 @@ Console.WriteLine();
 
 // 1. uzdevums - sasveicināšanās
 
-//string myGreting2 = "Kā tevi sauc?";
+string myGreting2 = "Kā tevi sauc?";
 
-//Console.WriteLine(myGreting2);
+Console.WriteLine(myGreting2);
 
-//string userName2 = Console.ReadLine();
-//Console.WriteLine("Sveiks, " + userName2);
+string userName2 = Console.ReadLine();
+Console.WriteLine("Sveiks, " + userName2);
 
 
 Console.WriteLine();
@@ -20,11 +20,11 @@ Console.WriteLine();
 
 // 2. uzdevums - int.Parse
 
-//Console.WriteLine("Kāds ir tavs vecums ?");
-//int userNumber = int.Parse(Console.ReadLine());
-//int addedToUserNumber = userNumber + 1;
-//bool adultNumber = addedToUserNumber >= 18;
-//Console.WriteLine($"Nākamgad tev paliks  {addedToUserNumber}! {adultNumber}, $Tu esi pilngadīgs";
+Console.WriteLine("Kāds ir tavs vecums ?");
+int userNumber = int.Parse(Console.ReadLine());
+int addedToUserNumber = userNumber + 1;
+bool adultNumber = addedToUserNumber >= 18;
+Console.WriteLine($"Nākamgad tev paliks  {addedToUserNumber}! {adultNumber}, $Tu esi pilngadīgs";
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -32,20 +32,20 @@ Console.WriteLine();
 
 // 3. uzdevums - Math.Max
 
-//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
-//int firstNumber = int.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadi otro skaitli");
-//int secondNumber = int.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadi trešo skaitli");
-//int thirdNumber = int.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadi ceturto skaitli");
-//int fourthNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi otro skaitli");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi trešo skaitli");
+int thirdNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi ceturto skaitli");
+int fourthNumber = int.Parse(Console.ReadLine());
 
-//int firstMaxNumber = Math.Max(firstNumber, secondNumber);
-//int secondMaxNumber = Math.Max(thirdNumber, fourthNumber);
-//int maxNumber = Math.Max(firstMaxNumber, secondMaxNumber);
+int firstMaxNumber = Math.Max(firstNumber, secondNumber);
+int secondMaxNumber = Math.Max(thirdNumber, fourthNumber);
+int maxNumber = Math.Max(firstMaxNumber, secondMaxNumber);
 
-//Console.WriteLine(maxNumber);
+Console.WriteLine(maxNumber);
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -53,18 +53,18 @@ Console.WriteLine();
 
 // 4. uzdevums - Math.Min
 
-//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
-//int firstNumber = int.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadi otro skaitli");
-//int secondNumber = int.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadi trešo skaitli");
-//int thirdNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi otro skaitli");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi trešo skaitli");
+int thirdNumber = int.Parse(Console.ReadLine());
 
-//int firstMinNumber = Math.Min(firstNumber, secondNumber);
-//int secondMinNumber = Math.Min(secondNumber, thirdNumber);
-//int minNumber = Math.Min(firstMinNumber, secondMinNumber);
+int firstMinNumber = Math.Min(firstNumber, secondNumber);
+int secondMinNumber = Math.Min(secondNumber, thirdNumber);
+int minNumber = Math.Min(firstMinNumber, secondMinNumber);
 
-//Console.WriteLine(minNumber);
+Console.WriteLine(minNumber);
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -73,14 +73,14 @@ Console.WriteLine();
 // 5. uzdevums - %
 
 
-//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
-//int firstNumber = int.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadi otro skaitli");
-//int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi otro skaitli");
+int secondNumber = int.Parse(Console.ReadLine());
 
-//int result = firstNumber % secondNumber;
+int result = firstNumber % secondNumber;
 
-//Console.WriteLine(result);
+Console.WriteLine(result);
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -88,11 +88,11 @@ Console.WriteLine();
 
 // 6. uzdevums - Odd or Even
 
-//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
-//int number = int.Parse(Console.ReadLine());
-//bool isEven = number % 2 == 0;
+Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+int number = int.Parse(Console.ReadLine());
+bool isEven = number % 2 == 0;
 
-//Console.WriteLine("Skaitlis ir pāra skaitlis:" + isEven);
+Console.WriteLine("Skaitlis ir pāra skaitlis:" + isEven);
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -101,13 +101,13 @@ Console.WriteLine();
 // 7. uzdevums - taisnstūra laukums
 
 
-//Console.WriteLine("Lūdzu, ievadiet pirmo skaitli");
-//double firstRoundableNumber = double.Parse(Console.ReadLine());
-//Console.WriteLine("Lūdzu, ievadiet otro skaitli");
-//double secondRoundalbeNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadiet pirmo skaitli");
+double firstRoundableNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadiet otro skaitli");
+double secondRoundalbeNumber = double.Parse(Console.ReadLine());
 
-//double area = Math.Round((firstRoundableNumber * secondRoundalbeNumber), 2);
-//Console.WriteLine("Taisnstūra laukums: " + area);
+double area = Math.Round((firstRoundableNumber * secondRoundalbeNumber), 2);
+Console.WriteLine("Taisnstūra laukums: " + area);
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -115,11 +115,11 @@ Console.WriteLine();
 
 // 8. uzdevums - taisnleņķa trijstūra laukuma aprēķināšana
 
-//Console.WriteLine("Lūdzu, ievadi trijstūra malas garumu");
-//int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Lūdzu, ievadi trijstūra malas garumu");
+int number = int.Parse(Console.ReadLine());
 
-//int area = number * number / 2;
-//Console.WriteLine(area);
+int area = number * number / 2;
+Console.WriteLine(area);
 
 Console.WriteLine();
 Console.WriteLine("======================================================");
@@ -127,11 +127,11 @@ Console.WriteLine();
 
 // 9. uzdevums - interpolācija
 
-//Console.WriteLine("Lūdzu ievadi savu vārdu");
-//string userName = Console.ReadLine();
+Console.WriteLine("Lūdzu ievadi savu vārdu");
+string userName = Console.ReadLine();
 
-//Console.WriteLine("Lūdzu ievadi savu veumu");
-//int userAge = int.Parse(Console.ReadLine());
-//Console.WriteLine($"Sveiks {userName}, tavs vecums ir {userAge}.");
+Console.WriteLine("Lūdzu ievadi savu veumu");
+int userAge = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sveiks {userName}, tavs vecums ir {userAge}.");
 
 
